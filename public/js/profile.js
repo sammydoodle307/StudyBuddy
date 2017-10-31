dd82b54ae973ec206a3ade19002f13d9fcb161f1
Jack Venberg <jvenberg@gmail.com>
2017-10-31 13:24:13 -0700

    Added start of profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,9 +7,10 @@ firebase.auth().onAuthStateChanged(function(user) {
     var isAnonymous = user.isAnonymous;
     var uid = user.uid;
     var providerData = user.providerData;
-   document.getElementById("account-info").innerHTML += "<p>displayName: " + displayName + "</p>";
-   document.getElementById("account-info").innerHTML += "<p>email: " + email + "</p>";
-   document.getElementById("account-info").innerHTML += "<p>User ID: " + uid + "</p>";
-   document.getElementById("account-info").innerHTML += "<p>photoURL: <img style='width:50%' src='" + photoURL + "'></p>";
+   // document.getElementById("account-info").innerHTML += "<p>displayName: " + displayName + "</p>";
+   // document.getElementById("account-info").innerHTML += "<p>email: " + email + "</p>";
+   // document.getElementById("account-info").innerHTML += "<p>User ID: " + uid + "</p>";
+   // document.getElementById("account-info").innerHTML += "<p>photoURL: <img style='width:50%' src='" + photoURL + "'></p>";
+   document.getElementById("profile-pic").src = photoURL;
   }
 });
